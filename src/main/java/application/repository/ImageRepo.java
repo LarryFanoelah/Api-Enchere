1d87cde6242936bb815bfd9ece356c1c2c3b05b7
DiamondraRH <rambelosondidy@gmail.com>
2023-02-03 06:52:11 +0300

    Delete ImageRepo.java

--- src/main/java/application/repository/ImageRepo.java
+++
@@ -1,7 +0,0 @@
-package application.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import application.models.Image;
-
-public interface ImageRepo extends JpaRepository<Image, Integer> {
-}
