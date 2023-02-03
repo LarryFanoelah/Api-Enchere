754fffeb3a3b28b5dba6ace1e00c11ee46edfe91
LarryFanoelah <99284028+LarryFanoelah@users.noreply.github.com>
2023-02-03 10:24:42 +0300

    Update LotRepo.java

--- src/main/java/application/repository/LotRepo.java
+++ src/main/java/application/repository/LotRepo.java
@@ -3,6 +3,8 @@ package application.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Modifying;
+import javax.transaction.Transactional;
 import java.util.List;
 import application.models.Lot;
 import application.models.Message;
@@ -17,6 +19,8 @@ public interface LotRepo extends JpaRepository<Lot, Integer> {
         @Query(value = "SELECT * FROM Lot WHERE id_utilisateur = :id_user", nativeQuery = true)
         public List<Lot> myEnchere(@Param("id_user") int id);
 
+        @Transactional
+        @Modifying
         @Query(value = "insert into lot(duree,prix_minimal,id_utilisateur,nom, descri,statut,id_categorie) values (?1,?2,?3,?4,?5,?6,?7)", nativeQuery = true)
         public Message insertEnchere (int duree, Double prixMinimal,int utilisateur,String nom,String descri,boolean statut, int categorie  );
 
